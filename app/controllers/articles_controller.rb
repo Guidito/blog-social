909785365bf397e780267a6099b6d8448a0c294d
guidito <guidomgm@gmail.com>
2019-09-03 18:08:29 -0500

    Actualziar la redireccion del update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,12 +22,12 @@ class ArticlesController < ApplicationController
 
     def create
         @article = Article.create(title: params[:article][:title], content: params[:article][:content])
-        redirect_to root_path
+        redirect_to articles_path
     end
 
     def destroy
         @article.destroy
-        redirect_to root_path
+        redirect_to articles_path
     end
 
     def find_article
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   get 'articles', to: 'articles#index'
   get 'articles/new', to: 'articles#new', as: :new_articles
-  get 'articles/:id', to: 'articles#show', as: :list_articles
+  get 'articles/:id', to: 'articles#show'
   get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
 
   patch 'articles/:id', to: 'articles#update', as: :article
