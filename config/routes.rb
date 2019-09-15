5a8eacd48e193a2f47625ddbdbbc8de25f6fad1f
Guido <contact@fazt.com>
2019-09-15 12:33:12 -0500

    Actualizacion de avatar para el perfil

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,27 @@
 Rails.application.routes.draw do
+
+  resources :categories
   devise_for :users
   #get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #get '/bienvenida', to: 'home#index'
+  get '/perfil', to: 'users#edit'
+  resources :users, only: [:update]
 
   root to: 'home#index'
 
-  get 'articles/user/:user_id', to: 'articles#from_author'
+  #get 'articles/user/:user_id', to: 'articles#from_author'
 
-  get 'articles', to: 'articles#index'
-  get 'articles/new', to: 'articles#new', as: :new_articles
-  get 'articles/:id', to: 'articles#show'
-  get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
+  resources :articles do
+    get "user/:user_id", to: 'articles#from_author', on: :collection
+  end
 
-  patch 'articles/:id', to: 'articles#update', as: :article
-  post 'articles/', to: 'articles#create'
-  delete 'articles/:id', to: 'articles#destroy'
+  #get 'articles', to: 'articles#index'
+  #get 'articles/new', to: 'articles#new', as: :new_articles
+  #get 'articles/:id', to: 'articles#show'
+  #get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
+
+  #patch 'articles/:id', to: 'articles#update', as: :article
+  #post 'articles/', to: 'articles#create'
+  #delete 'articles/:id', to: 'articles#destroy'
 end
